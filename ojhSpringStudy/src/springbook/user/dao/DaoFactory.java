bede8bbb407607cee68dfc54e39bff9d08530ac0
uuin99 <uuin99@gmail.com>
2013-12-18 00:53:12 +0900

    1.4 제어의 역전(IoC)

---
+++ ojhSpringStudy/src/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package springbook.user.dao;
+
+public class DaoFactory {
+	public UserDao userDao() {
+		UserDao userDao = new UserDao(connectionMaker());
+		return userDao;
+	}
+
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
+}
